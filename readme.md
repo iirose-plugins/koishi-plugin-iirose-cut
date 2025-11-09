2a852b6309ebdaf4d4b6a36befe8460d92c0e973
shangxueink <1919892171@qq.com>
2025-11-09 17:40:21 +0800

    init

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-iirose-cut?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-iirose-cut)
 
-蔷薇花园自动切除退房的人所点的歌曲
+用于蔷薇花园切除退房的人所点的歌曲
--- src/index.ts
+++ src/index.ts
@@ -1,113 +1,65 @@
 import { Context, Schema } from 'koishi';
-import { } from 'koishi-plugin-adapter-iirose';
 
-export const name = 'iirose-cut';
+export const name = 'iirose-cut-song';
 
-export interface Config { }
+export const inject = ['database'];
 
-export const Config: Schema<Config> = Schema.object({});
+export const usage = `
+---
 
-export function apply(ctx: Context)
+BOT需要有房间成员的星标权限 / 管理权限。
+
+如果你的bot没有权限，则可以发起cut投票，请使用 iirose-self-cut 插件。
+
+---
+
+使用指令以切歌：
+
+- iirose.media.cut ： 终止当前歌曲
+- iirose.media.cutall ： 终止所有歌曲
+
+---
+
+本插件需要调用者需要至少2级权限。
+
+如果你的权限不够，请使用 change-auth-callme 插件来提权。
+
+务必不要使用1级权限，否则可能会导致
+
+---
+`;
+
+export interface Config
 {
-  const nowUserList = [];
-  const whiteList = [];
-  let status = false;
-
-  ctx.on('iirose/newMusic', (session, data) =>
-  {
-    if (!status) { return; }
-
-    const index = nowUserList.indexOf(data.owner);
-    const index2 = whiteList.indexOf(data.owner);
-
-    if (index > -1 || index2 > -1) { return; }
-
-    ctx.emit('iirose/cut-one', {});
-  });
-
-  ctx.on('iirose/joinRoom', (session, data) =>
-  {
-    const index = nowUserList.indexOf(data.username);
-
-    if (index > -1) { return; }
-    nowUserList.push(data.username);
-  });
-
-  ctx.on('iirose/leaveRoom', (session, data) =>
-  {
-    const index = nowUserList.indexOf(data.username);
-    if (index < 0) { return; }
-    nowUserList.splice(index, 1);
-  });
-  ctx.command('iirose', '花园工具');
-  ctx.command('iirose.cut', '花园自动切歌系统')
-    .option('add', '-a <艾特:string> 添加一个人到白名单', { type: /\[\*([\s\S]+)\*\]/ })
-    .option('del', '-d <艾特:string> 移除一个人到白名单', { type: /\[\*([\s\S]+)\*\]/ })
-    .option('on', '开启自动切歌')
-    .option('off', '关闭自动切歌')
-    .option('view', '查看当前的列表')
-    .usage('\n注意：机器人需要**星标**权限\n每次插件启动时，房间列表会初始化为空，只有当有人主动退出与进入等操作的时候，才会对房间列表进行操作')
-    .action(v =>
-    {
-      if (v.session.platform !== 'iirose') { return ' [IIROSE-CUT] 该平台不支持使用此插件'; }
-
-      if (v.options.hasOwnProperty('add'))
-      {
-        const at = getAt(v.options.add);
-
-        const index = whiteList.indexOf(at);
-        if (index > -1) { return '[IIROSE-CUT] 用户已存在白名单'; }
-
-        whiteList.push(at);
-        return '[IIROSE-CUT] 添加白名单成功';
-      }
-
-      if (v.options.hasOwnProperty('del'))
-      {
-        const at = getAt(v.options.del);
-
-        const index = whiteList.indexOf(at);
-        if (index < 0) { return '[IIROSE-CUT] 该用户不在白名单中'; }
-        whiteList.splice(index, 1);
-      }
-
-      if (v.options.hasOwnProperty('on'))
-      {
-        status = true;
-
-        return '[IIROSE-CUT] 自动切歌已启动';
-      }
-
-      if (v.options.hasOwnProperty('off'))
-      {
-        status = false;
-
-        return '[IIROSE-CUT] 自动切歌已关闭';
-      }
-
-
-      if (v.options.hasOwnProperty('view'))
-      {
-        const nowUserListView = '当前房间人员列表：\n' + nowUserList.map(data =>
-        {
-          return `\n [*${data}*] `;
-        });
-
-        const whiteListView = '当前白名单人员列表：' + whiteList.map(data =>
-        {
-          return `\n [*${data}*] `;
-        });
-
-        return '[IIROSE-CUT] \n\n' + nowUserListView + '\n\n' + whiteListView;
-      }
-    });
+  commandAuthority: number;
 }
 
-const getAt = (message: string) =>
-{
-  const reg = /\[\*([\s\S]+)\*\]/;
-  const a = reg.test(message);
-  if (!a) { return ''; }
+export const Config: Schema<Config> = Schema.object({
+  commandAuthority: Schema.number().role('slider').min(0).max(5).step(1).default(2).description("指令权限等级"),
+});
 
-  return message.match(reg)[1];
-};
+export function apply(ctx: Context, config: Config)
+{
+  ctx
+    .command('iirose.media.cut', '终止当前歌曲', { authority: config.commandAuthority })
+    .action(async ({ session }) =>
+    {
+      if (session.platform !== "iirose")
+      {
+        return "暂不支持其他平台。";
+      }
+      await session.bot.internal.cutOne();
+      return;
+    });
+  ctx
+    .command('iirose.media.cutall', '终止所有歌曲', { authority: config.commandAuthority })
+    .action(async ({ session }) =>
+    {
+      if (session.platform !== "iirose")
+      {
+        return "暂不支持其他平台。";
+      }
+      await session.bot.internal.cutAll();
+      return;
+    });
+}
